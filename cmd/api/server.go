50a80b6750e3b12267c63e628bd8579e65f62d34
silentFellow <gowtham.sri+github@zohomail.in>
2025-01-17 19:53:17 +0530

    Add templates and test it with routes

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -2,33 +2,39 @@ package api
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
 type server struct {
-  addr string
-  server *http.Server
+	addr   string
+	server *http.Server
 }
 
 func NewServer(addr string) *server {
-  return &server{
-    addr: addr,
-  }
+	return &server{
+		addr: addr,
+	}
 }
 
 func (s *server) Run() error {
-  port := fmt.Sprintf(":%v", s.addr)
+	port := fmt.Sprintf(":%v", s.addr)
 
-  router := http.NewServeMux()
-  subRouter := http.NewServeMux()
-  router.Handle("/api/v1/", http.StripPrefix("/api/v1", subRouter))
+	router := http.NewServeMux()
+	subRouter := http.NewServeMux()
+	router.Handle("/api/v1/", http.StripPrefix("/api/v1", subRouter))
 
-  middlewareStack := logginMiddleware(router)
+	subRouter.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		tmpl := template.Must(template.ParseFiles("templates/index.html"))
+		tmpl.Execute(w, nil)
+	})
 
-  s.server = &http.Server {
-    Addr: port,
-    Handler: middlewareStack,
-  }
+	middlewareStack := logginMiddleware(router)
 
-  return s.server.ListenAndServe()
+	s.server = &http.Server{
+		Addr:    port,
+		Handler: middlewareStack,
+	}
+
+	return s.server.ListenAndServe()
 }
