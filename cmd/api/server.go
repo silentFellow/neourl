73f0c7627e264019160a9fb39608e44a18f0d58a
silentFellow <gowtham.sri+github@zohomail.in>
2025-01-17 19:55:47 +0530

    Add auto-redirect from / route

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -21,6 +21,10 @@ func (s *server) Run() error {
 	port := fmt.Sprintf(":%v", s.addr)
 
 	router := http.NewServeMux()
+  router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+    http.Redirect(w, r, "/api/v1/", http.StatusMovedPermanently)
+  })
+
 	subRouter := http.NewServeMux()
 	router.Handle("/api/v1/", http.StripPrefix("/api/v1", subRouter))
 
